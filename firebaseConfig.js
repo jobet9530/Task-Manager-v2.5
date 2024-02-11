21c7383b70f63495773f1da6a7800be59d1839c7
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-02-11 07:33:24 +0000

    feat: Import necessary SDKs for Firebase and remove unused code

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBCq3y-SH8U4f8KQMcDu4zViSWvVGVsTLM",
   authDomain: "task-manager-ea560.firebaseapp.com",
@@ -11,7 +16,6 @@ const firebaseConfig = {
   measurementId: "G-DJGEVXEZ7F",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default analytics;
